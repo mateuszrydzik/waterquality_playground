d2b434e3cdf5762aa966f72a28b616998679cf1d
mateusz <62845725+mateuszrydzik@users.noreply.github.com>
2024-01-24 17:06:47 +0000

    finished example report

--- quarto-docs/example_report_download.R
+++ quarto-docs/example_report_download.R
@@ -16,3 +16,35 @@ imagery_2017 <- get_sentinel2_imagery(
     output_filename = "data/costarica_2017.tif"
 )
 ```
+``` {r}
+ndci_2023_lr <- calculate_index(rast_2023, aoi_32617[1, 0], "NDCI", sat="sentinel2")
+#ndti_2023_lr <- calculate_index(rast_2023, aoi_32617[1, 0], "NDTI", sat="sentinel2")
+sabi_2023_lr <- calculate_index(rast_2023, aoi_32617[1, 0], "Al10SABI", use_wq = TRUE, sat = "sentinel2")
+#turb_2023_lr <- calculate_index(rast_2023, aoi_32617[1, 0], "TurbFrohn09GreenPlusRedBothOverBlue", use_wq = TRUE, sat = "sentinel2")
+
+ndci_2017_lr <- calculate_index(rast_2017, aoi_32617[1, 0], "NDCI", sat="sentinel2")
+#ndti_2017_lr <- calculate_index(rast_2017, aoi_32617[1, 0], "NDTI", sat="sentinel2")
+sabi_2017_lr <- calculate_index(rast_2017, aoi_32617[1, 0], "Al10SABI", use_wq = TRUE, sat = "sentinel2")
+#turb_2017_lr <- calculate_index(rast_2017, aoi_32617[1, 0], "TurbFrohn09GreenPlusRedBothOverBlue", use_wq = TRUE, sat = "sentinel2")
+
+ndci_2023_north <- calculate_index(rast_2023, aoi_32617[5, 0], "NDCI", sat="sentinel2")
+#ndti_2023_north <- calculate_index(rast_2023, aoi_32617[5, 0], "NDTI", sat="sentinel2")
+sabi_2023_north <- calculate_index(rast_2023, aoi_32617[5, 0], "Al10SABI", use_wq = TRUE, sat = "sentinel2")
+#turb_2023_north <- calculate_index(rast_2023, aoi_32617[5, 0], "TurbFrohn09GreenPlusRedBothOverBlue", use_wq = TRUE, sat = "sentinel2")
+
+ndci_2017_north <- calculate_index(rast_2017, aoi_32617[5, 0], "NDCI", sat="sentinel2")
+#ndti_2017_north <- calculate_index(rast_2017, aoi_32617[5, 0], "NDTI", sat="sentinel2")
+sabi_2017_north <- calculate_index(rast_2017, aoi_32617[5, 0], "Al10SABI", use_wq = TRUE, sat = "sentinel2")
+#turb_2017_north <- calculate_index(rast_2017, aoi_32617[5, 0], "TurbFrohn09GreenPlusRedBothOverBlue", use_wq = TRUE, sat = "sentinel2")
+
+ndci_2023_south <- calculate_index(rast_2023, aoi_32617[2, 0], "NDCI", sat="sentinel2")
+#ndti_2023_south <- calculate_index(rast_2023, aoi_32617[2, 0], "NDTI", sat="sentinel2")
+sabi_2023_south <- calculate_index(rast_2023, aoi_32617[2, 0], "Al10SABI", use_wq = TRUE, sat = "sentinel2")
+#turb_2023_south <- calculate_index(rast_2023, aoi_32617[2, 0], "TurbFrohn09GreenPlusRedBothOverBlue", use_wq = TRUE, sat = "sentinel2")
+
+ndci_2017_south <- calculate_index(rast_2017, aoi_32617[2, 0], "NDCI", sat="sentinel2")
+#ndti_2017_south <- calculate_index(rast_2017, aoi_32617[2, 0], "NDTI", sat="sentinel2")
+sabi_2017_south <- calculate_index(rast_2017, aoi_32617[2, 0], "Al10SABI", use_wq = TRUE, sat = "sentinel2")
+#turb_2017_south <- calculate_index(rast_2017, aoi_32617[2, 0], "TurbFrohn09GreenPlusRedBothOverBlue", use_wq = TRUE, sat = "sentinel2")
+
+```
